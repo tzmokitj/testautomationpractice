7367587535ee579d7a6698f3cfca71b64ff08261
DESKTOP-K2CCN2L\Uros <zackwtf@hotmail.com>
2022-10-02 12:39:43 +0200

    Initial Commit

---
+++ TestAutomationPractice/Features/ContactUs.feature
@@ -0,0 +1,13 @@
+﻿Feature: ContactUs
+	In order to contact customer service
+	As a user
+	I want to be able to use contact us form
+
+
+@mytag
+Scenario: User can contact customer service
+	Given user opens contact us page
+	When user fill in all required fields with 'Webmaster' heading and 'QA Kurs' message
+	And user submits the form
+	Then message 'Your message has been successfully sent to our team.' is presented to the user
+
---
+++ TestAutomationPractice/Steps/SearchSteps.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+using TestAutomationPractice.Helpers;
+using TestAutomationPractice.Pages;
+
+namespace TestAutomationPractice.Steps
+{
+    [Binding]
+    public class SearchSteps : Base
+    {
+        Utilities ut = new Utilities(Driver);
+        HomePage hp = new HomePage(Driver);
+        
+        [Given(@"user enters '(.*)' search term")]
+        public void GivenUserEntersSearchTerm(string term)
+        {
+            ut.EnterTextInElement(hp.searchField, term);
+        }
+        
+        [When(@"user submits the search")]
+        public void WhenUserSubmitsTheSearch()
+        {
+            ut.ClickOnElement(hp.searchBtn);
+        }
+        
+        [Then(@"results for '(.*)' search term are displayed")]
+        public void ThenResultsForSearchTermAreDisplayed(string term)
+        {
+            SearchResultsPage srp = new SearchResultsPage(Driver);
+            Assert.That(ut.ReturnTextFromElement(srp.searchResult), Does.Contain(term), "Wrong search resulsts are displayed");
+
+        }
+    }
+}
